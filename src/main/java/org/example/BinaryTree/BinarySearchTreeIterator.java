a8d0630549560cbaed49e117d3839804b2d82e13
Piotr Tatarczuk <93190940+ptatarczuk@users.noreply.github.com>
2023-11-27 21:04:26 +0100

    Add solution to BinarySearchTreeIterator

---
+++ src/main/java/org/example/BinaryTree/BinarySearchTreeIterator.java
@@ -0,0 +1,27 @@
+package org.example.BinaryTree;
+
+import java.util.Stack;
+
+public class BinarySearchTreeIterator {
+    public class BSTIterator {
+        private Stack<TreeNode> stack = new Stack<TreeNode>();
+
+        public BSTIterator(TreeNode root) {
+            pushAll(root);
+        }
+
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        public int next() {
+            TreeNode tmpNode = stack.pop();
+            pushAll(tmpNode.right);
+            return tmpNode.val;
+        }
+
+        private void pushAll(TreeNode node) {
+            for (; node != null; stack.push(node), node = node.left);
+        }
+    }
+}
