b8d01a9e8dba12ea3bd5f248c3101beb4a5a9485
Piotr Tatarczuk <93190940+ptatarczuk@users.noreply.github.com>
2023-12-19 19:06:23 +0100

    Add solution to CombinationSum

---
+++ src/main/java/org/example/Backtracking/CombinationSum.java
@@ -0,0 +1,29 @@
+package org.example.Backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum {
+    List<List<Integer>> ans = new ArrayList<>();
+    ArrayList<Integer> ls = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum(int[] c, int target) {
+        cum(c, target, 0);
+        return ans;
+    }
+
+    public void cum(int[] c, int target, int start) {
+        if (target == 0) {
+            ans.add(new ArrayList<>(ls));
+            return;
+        }
+
+        for (int i = start; i < c.length; i++) {
+            if (c[i] <= target) {
+                ls.add(c[i]);
+                cum(c, target - c[i], i);
+                ls.remove(ls.size() - 1);
+            }
+        }
+    }
+}
---
+++ src/test/java/org/example/Backtracking/CombinationSumTest.java
@@ -0,0 +1,6 @@
+package org.example.Backtracking;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CombinationSumTest {
+    }
