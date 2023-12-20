14a77e315cc5c99e355fd0fcb8f7bfc3c3d4149a
Piotr Tatarczuk <93190940+ptatarczuk@users.noreply.github.com>
2023-12-20 20:02:48 +0100

    Add tests for GenerateParenthese

--- src/test/java/org/example/Backtracking/GenerateParenthesesTest.java
+++ src/test/java/org/example/Backtracking/GenerateParenthesesTest.java
@@ -1,8 +1,30 @@
 package org.example.Backtracking;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GenerateParenthesesTest {
+    @Test
+    public void testGenerateParenthesisExample1() {
+        GenerateParentheses generator = new GenerateParentheses();
+        int n = 3;
+        List<String> expected = Arrays.asList("((()))", "(()())", "(())()", "()(())", "()()()");
+        List<String> result = generator.generateParenthesis(n);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testGenerateParenthesisExample2() {
+        GenerateParentheses generator = new GenerateParentheses();
+        int n = 1;
+        List<String> expected = Arrays.asList("()");
+        List<String> result = generator.generateParenthesis(n);
+        assertEquals(expected, result);
+    }
 
 
 }
