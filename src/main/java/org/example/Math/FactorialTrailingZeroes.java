8921f2648f660b2c0092dabc10099bb8724f1454
Piotr Tatarczuk <93190940+ptatarczuk@users.noreply.github.com>
2024-01-28 13:18:32 +0100

    Add solution for FactorialTrailingZeroes

---
+++ src/main/java/org/example/Math/FactorialTrailingZeroes.java
@@ -0,0 +1,12 @@
+package org.example.Math;
+
+public class FactorialTrailingZeroes {
+    public int trailingZeroes(int n) {
+        int r = 0;
+        while (n > 0) {
+            n /= 5;
+            r += n;
+        }
+        return r;
+    }
+}
