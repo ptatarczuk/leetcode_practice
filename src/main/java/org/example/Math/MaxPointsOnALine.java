4f7e1573665dc32c9baeb51555cf9ad5fef152e5
Piotr Tatarczuk <93190940+ptatarczuk@users.noreply.github.com>
2024-02-01 08:27:26 +0100

    Add solution for MaxPointsOnALine

---
+++ src/main/java/org/example/Math/MaxPointsOnALine.java
@@ -0,0 +1,27 @@
+package org.example.Math;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaxPointsOnALine {
+    public int maxPoints(int[][] points) {
+        int n = points.length;
+        if(n <= 2) return n;
+        int ans = 2;
+
+        for(int[] a : points){
+            Map<Double, Integer> map = new HashMap<>();
+            for(int[] b : points){
+                if(a == b) continue;
+                double slope = 0;
+                if(b[0] - a[0] == 0) slope = Double.POSITIVE_INFINITY;
+                else slope = (b[1] -a[1]) / (double)(b[0] - a[0]);
+
+                map.put(slope, map.getOrDefault(slope, 1)+1);
+                ans = Math.max(ans, map.get(slope));
+            }
+        }
+        return ans;
+
+    }
+}
