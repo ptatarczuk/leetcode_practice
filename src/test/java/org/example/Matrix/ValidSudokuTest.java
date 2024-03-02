bab758c619b42a0a1d758d242314324fa4fa2252
Piotr Tatarczuk <93190940+ptatarczuk@users.noreply.github.com>
2024-03-02 17:30:26 +0100

    Add tests for ValidSudoku

---
+++ src/test/java/org/example/Matrix/ValidSudokuTest.java
@@ -0,0 +1,46 @@
+package org.example.Matrix;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidSudokuTest {
+    @Test
+    public void testValidSudokuExample1() {
+        ValidSudoku validator = new ValidSudoku();
+
+        char[][] board = {
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        assertTrue(validator.isValidSudoku(board));
+    }
+
+    @Test
+    public void testInvalidSudokuExample2() {
+        ValidSudoku validator = new ValidSudoku();
+
+        char[][] board = {
+                {'8','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+
+        assertFalse(validator.isValidSudoku(board));
+    }
+
+}
